7269715d56dd02a33e62d74df86b77476f5ea973
Matthew Kwong <matthewkwong2@gmail.com>
2022-07-18 14:30:03 +0800

    feat: add `query` in `gatsby-plugin-sitemap` options

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,11 +5,23 @@ const config: GatsbyConfig = {
     title: `sitemap-typegen`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sitemap"]
+  plugins: [
+    {
+      resolve: "gatsby-plugin-sitemap",
+      options: {
+        query: `
+          query Sitemap {
+            site {
+              siteMetadata {
+                siteUrl
+              }
+            }
+          }        
+        `,
+      }
+    }
+  ]
 };
 
 export default config;
